3c0ded03a56abd16e08684c32584252481a23354
lianhong-tg <lianhong.lim@tazte.co>
2026-08-02 14:54:51 +0800

    Amend next config files

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // static export for GitHub Pages
   devIndicators: false,
+  basePath: "/Hong",
+  assetPrefix: "/Hong/",
+  trailingSlash: true, // generate URLs and file paths with a trailing slash
   turbopack: {
     root: process.cwd(),
   },
